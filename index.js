2a6c2ee809439a93c34a74217b018b5a7234bd78
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2019-12-16 14:52:29 -0600

    CLASE 8 - Archivos publicos y estaticos

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import morgan from 'morgan';
 //cors: permite recibir solicitudes remotas, no solo desde la misma PC
 import cors from 'cors'
 //const cors = require('cors');
+import path from 'path';
 
 const app=express();
 app.use(morgan('dev'));
@@ -16,10 +17,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//ruta de los archivos estaticos
+app.use(express.static(path.join(__dirname,'public')));
+
 //para cuando se despliegue la aplicacion, reciba el puerto asignado automaticamente por el servidor
 //de lo contrario toma el puerto por defecto 3000
 app.set('port',process.env.PORT || 3000);
 
 app.listen(app.get('port'), ()=>{
     console.log('server on port '+app.get('port'));
+    //console.log(path.join(__dirname,'public'));
 }); 
