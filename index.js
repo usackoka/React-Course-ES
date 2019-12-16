e826c3896d3da3acdb827d99848090a5deab62a4
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2019-12-16 13:59:50 -0600

    CLASE 6 - Morgan
    Se logran visualizar las peticiones get desde el navegador

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express=require('express');
+const morgan = require('morgan');
+
 const app=express();
+app.use(morgan('dev'));
 
 //para cuando se despliegue la aplicacion, reciba el puerto asignado automaticamente por el servidor
 //de lo contrario toma el puerto por defecto 3000
@@ -7,4 +10,4 @@ app.set('port',process.env.PORT || 3000);
 
 app.listen(app.get('port'), ()=>{
     console.log('server on port '+app.get('port'));
-});
+}); 
