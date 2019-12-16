41809ba0409f840ee9823c1f4c902f8b90d2a039
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2019-12-16 14:09:30 -0600

    CLASE 6 - cors

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express=require('express');
+//morgan: administrar peticiones desde el servidor o cliente
 const morgan = require('morgan');
+//cors: permite recibir solicitudes remotas, no solo desde la misma PC
+const cors = require('cors');
 
 const app=express();
 app.use(morgan('dev'));
+app.use(cors());
+
+//para que admita recibir solicitudes en formato JSON
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //para cuando se despliegue la aplicacion, reciba el puerto asignado automaticamente por el servidor
 //de lo contrario toma el puerto por defecto 3000
