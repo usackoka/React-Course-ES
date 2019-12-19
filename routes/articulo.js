f10f885853a4095c47111c9b021f9d59aac2e6ce
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2019-12-19 12:37:17 -0600

    CLASE 35 - Buscar por codigo de barras

--- routes/articulo.js
+++ routes/articulo.js
@@ -6,6 +6,7 @@ const router = routerx();
 
 router.post('/add',auth.verifyAlmacenero,ArticuloController.add);
 router.get('/query',auth.verifyAlmacenero,ArticuloController.query);
+router.get('/queryCodigo',auth.verifyUsuario,ArticuloController.queryCodigo);
 router.get('/list',auth.verifyAlmacenero,ArticuloController.list);
 router.put('/update',auth.verifyAlmacenero,ArticuloController.update);
 router.delete('/remove',auth.verifyAlmacenero,ArticuloController.remove);
