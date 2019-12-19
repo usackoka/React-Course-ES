b7ac260c410423175a42b8d2524c2ef34d2f8085
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2019-12-19 14:16:32 -0600

    CLASE 37 - Modelo Venta

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import Categoria from './categoria';
 import Articulo from './articulo'
 import Usuario from './usuario'
 import Persona from './persona'
+import Venta from './venta'
 
 export default{
     Categoria,
     Articulo,
     Usuario,
-    Persona
+    Persona,
+    Venta
 }
